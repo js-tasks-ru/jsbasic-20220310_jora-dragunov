725f9918de5caf75aae8614bee308b658256a564
GeorgDragunov <100476927+GeorgDragunov@users.noreply.github.com>
2022-03-18 11:47:21 +0400

    Update index.js

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,9 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (name.length > 4 && name !== null && name !== ' '){
-    return true;
-  } else false;
+  return (name !== null && name.length >= 4 &&  name !== ' ');
 }
 
 function sayHello() {
